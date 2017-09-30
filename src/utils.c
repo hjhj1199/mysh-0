4901fff73ba23955ca1436b670ed5e46cb0875fd
Kim Hyung Chan <hj1003hj@ajou.ac.kr>
2017-10-01 02:56:40 +0900

    completed utils.c taht is pasing code!

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,27 @@
+#include <string.h>
+#include <stdlib.h>
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+    char usercommand[8096],*token=NULL;
+    int cnt=0;
+
+    strcpy(usercommand,command);
+  
+    *argv=(char**)malloc(4*sizeof(char));
+
+    token=strtok(usercommand," \n\t");
+
+    do
+    {
+        *(*argv+cnt)=(char*)malloc(strlen(token+1)*sizeof(char));
+        strcpy(*(*argv+cnt),token);
+        cnt++;
+        token=strtok(NULL," \n\t");
+        if (token==NULL) break;
+    }while(1);
+    
+    *argc=cnt;
 }
