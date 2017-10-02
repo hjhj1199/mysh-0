9688f43867455ae4f54c5f0d97f774848399ba8a
Kim Hyung Chan <hj1003hj@ajou.ac.kr>
2017-10-02 23:08:55 +0900

    completed unit test, edited the function test

--- src/commands.c
+++ src/commands.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
@@ -11,10 +12,12 @@ int do_cd(int argc, char** argv) {
 }
 
 int do_pwd(int argc, char** argv) {
-  char *location;
+  char location[8096];
   if (!validate_pwd_argv(argc, argv))
     return -1;
   getcwd(location,8096);
+  
+  printf("%s\n",location);
   // TODO: Fill it!
 
   return 0;
@@ -22,7 +25,7 @@ int do_pwd(int argc, char** argv) {
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  if ((argc==2 && strcmp(argv[0],"cd")==0 && strcmp(argv[1],"..")==0) || (argc==2 && strcmp(argv[0],"cd")==0 && strcmp(argv[1],".")==0)) return 1;
+  if ((argc==2 && strcmp(argv[0],"cd")==0) || (argc==2 && strcmp(argv[0],"cd")==0)) return 1;
   return 0;
 }
 
