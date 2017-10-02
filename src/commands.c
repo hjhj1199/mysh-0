34b2b0e021809e4c296e3a47c16a2072191fb1cf
Kim Hyung Chan <hj1003hj@ajou.ac.kr>
2017-10-02 21:42:35 +0900

    edited the cd and pwd!

--- src/commands.c
+++ src/commands.c
@@ -1,19 +1,20 @@
 #include <string.h>
-
+#include <unistd.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
+  chdir(argv[1]);
   // TODO: Fill it!
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
+  char *location;
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
+  getcwd(location,8096);
   // TODO: Fill it!
 
   return 0;
@@ -21,10 +22,12 @@ int do_pwd(int argc, char** argv) {
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if ((argc==2 && strcmp(argv[0],"cd")==0 && strcmp(argv[1],"..")==0) || (argc==2 && strcmp(argv[0],"cd")==0 && strcmp(argv[1],".")==0)) return 1;
+  return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
+  if (argc==1 && strcmp(argv[0],"pwd")==0) return 1;
   // TODO: Fill it!
-  return 1;
+  return 0;
 }
